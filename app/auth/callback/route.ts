e0a227e1fc3e01bd9e6d6ec5d814f9baf5cf8c84
blake-ekelund <blake.ekelund@workchores.com>
2026-01-21 21:39:27 -0500

    added sign in

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -21,7 +21,11 @@ export async function GET(request: Request) {
           },
           setAll(cookiesToSet) {
             cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options);
+              cookieStore.set(name, value, {
+                ...options,
+                sameSite: "lax",
+                secure: true,
+              });
             });
           },
         },
