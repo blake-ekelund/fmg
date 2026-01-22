13c07f4f1c33b4b527428204be86951370066c67
blake-ekelund <blake.ekelund@workchores.com>
2026-01-21 23:14:51 -0500

    added sign in

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,23 +1,14 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import { createBrowserClient } from "@supabase/ssr";
 
-export async function createClient() {
-  const cookieStore = await cookies();
-
-  return createServerClient(
+export function createClient() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            // ❗ DO NOT override options
-            cookieStore.set(name, value, options);
-          });
-        },
+      auth: {
+        detectSessionInUrl: true,
+        persistSession: true,
+        autoRefreshToken: true,
       },
     }
   );
